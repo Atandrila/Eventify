e6fbc3d7dfe119724aaf9799aef4e7699c9d662d
mantakanusaiba <123790428+mantakanusaiba@users.noreply.github.com>
2025-08-24 16:13:09 +0000

    vercel package

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ Add this to fix JS/CSS loading issue on Vercel
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5000/',
         changeOrigin: true,
       },
     },
   },
 })
+
+ 
