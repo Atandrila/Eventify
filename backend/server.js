aaa40e540525710eaf1548a16a4d8581a64cc1a7
mantakanusaiba <123790428+mantakanusaiba@users.noreply.github.com>
2025-08-24 16:34:31 +0000

    backend package

--- backend/server.js
+++ backend/server.js
@@ -9,35 +9,21 @@ const registrationRoutes = require('./routes/registrationRoutes');
 
 dotenv.config();
 
-const app = express(); 
+const app = express();
 
 // CORS config
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true
 }));
 
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173'];
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, curl requests)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
 
 // Middleware
 app.use(express.json());
@@ -47,6 +33,4 @@ app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/registrations', registrationRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+module.exports = app;
